bfb315008c3a1a8513194663c63765bf0969af38
Jason Gilmore <wj@wjgilmore.com>
2020-12-15 17:00:42 -0500

    Added post-process transformation script

---
+++ src/transform.php
@@ -0,0 +1,17 @@
+//========================
+//Transform a response
+//======================== 
+$responseBody = $event['response']['content'];
+
+foreach ($responseBody['resource'] as $n => $record) {
+	$record["name"] = [
+	    "first" => $record["first_name"],
+	    "last" =>  $record["last_name"]
+	];
+	unset($record["first_name"]);
+	unset($record["last_name"]);
+    unset($record["hire_date"]);
+	$responseBody['resource'][$n] = $record;
+}
+
+$event['response']['content'] = $responseBody;
